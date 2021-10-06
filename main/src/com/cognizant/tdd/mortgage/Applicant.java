3376f4be8475b1b301d5eaa6c130ab0808c63d1f
mahjoobah999 <48362108+mahjoobah999@users.noreply.github.com>
2021-10-06 11:41:46 -0500

    third test case commit (testSeeStatus)

--- main/src/com/cognizant/tdd/mortgage/Applicant.java
+++ main/src/com/cognizant/tdd/mortgage/Applicant.java
@@ -3,9 +3,6 @@ package com.cognizant.tdd.mortgage;
 public class Applicant {
 
     private int loan_amount;
-//    private boolean approve;
-//    private boolean accept;
-
     private double requested_amount;
     private int dti;
     private int credit_score;
